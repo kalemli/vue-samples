ffda0bd1b12dab4d7bd01a00d838381e6577db80
Atif Galamov <kalemli@gmail.com>
2019-08-25 16:41:31 +0400

    initial codes

---
+++ backend/src/main/java/com/vuesamples/VueSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.vuesamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSamplesApplication.class, args);
+	}
+
+}
